6a2cc23f5cedef4b5815928ae3d0e628de649a20
The Lunar Carver <ethancarnahan@gmail.com>
2025-12-13 11:15:34 -0800

    Add project files.

---
+++ BPMHelper/Plugin.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Beatmap.Base;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace BPMHelper
+{
+    [Plugin("BPM Helper")]
+    public class BPMHelper
+    {
+        private const int EditorSceneBuildIndex = 3;
+        private const int PollIntervalMilliseconds = 1000; // poll interval
+        private float numberOfBeats = 1;
+
+        private UI ui = null;
+        private MapEditorUI mapEditorUI = null;
+        private BPMChangeGridContainer bpmChangeGridContainer = null;
+        private AudioTimeSyncController audioTimeSyncController = null;
+
+        [Init]
+        private void Init()
+        {
+            SceneManager.sceneLoaded += SceneLoaded;
+            ui = new UI(this);
+            Debug.Log("BPM Helper has loaded!");
+        }
+
+        private async void SceneLoaded(Scene scene, LoadSceneMode mode) {
+            if (scene.buildIndex != EditorSceneBuildIndex)
+                return;
+
+            await FindGameObjects();
+        }
+
+        // Places 1000bpm at the cursor
+        public void AddInitialBPM()
+        {
+            float currentTime = audioTimeSyncController.CurrentJsonTime;
+            BaseBpmEvent bpmEvent = new BaseBpmEvent(currentTime, 1000f);
+            bpmChangeGridContainer.SpawnObject(bpmEvent);
+            bpmChangeGridContainer.RefreshModifiedBeat();
+        }
+
+        // Changes the closest bpm behind the cursor to (60 * numberOfBeats / (cursor time - last bpm time))
+        // Spawns error dialog box if there is no bpm event behind cursor
+        public void AddFinalBPM()
+        {
+            float currentTime = audioTimeSyncController.CurrentJsonTime;
+            BaseBpmEvent bpmEvent = GetClosestBehindBPMEvent(currentTime);
+            if (bpmEvent == null) {
+                SpawnErrorBox("No BPM events found behind cursor");
+                return;
+            }
+
+            float newBPM = 60 * numberOfBeats / (currentTime - bpmEvent.JsonTime);
+            bpmEvent.JsonTime = newBPM;
+            bpmChangeGridContainer.RefreshModifiedBeat();
+        }
+
+        // Does AddFinalBPM and AddInitialBPM together
+        public void AddMiddleBPM()
+        {
+            AddFinalBPM();
+            AddInitialBPM();
+        }
+
+        public void UpdateNumberOfBeats(string newBeats)
+        {
+            numberOfBeats = float.Parse(newBeats);
+        }
+
+        private async Task FindGameObjects()
+        {
+            while (mapEditorUI == null || bpmChangeGridContainer == null || audioTimeSyncController == null)
+            { 
+                await Task.Delay(PollIntervalMilliseconds);
+                mapEditorUI = mapEditorUI ?? UnityEngine.Object.FindFirstObjectByType<MapEditorUI>();
+                bpmChangeGridContainer = bpmChangeGridContainer ?? UnityEngine.Object.FindFirstObjectByType<BPMChangeGridContainer>();
+                audioTimeSyncController = audioTimeSyncController ?? UnityEngine.Object.FindFirstObjectByType<AudioTimeSyncController>();
+            }
+        }
+
+        private BaseBpmEvent GetClosestBehindBPMEvent(float currentTime)
+        {
+            IEnumerable<BaseBpmEvent> bpmEvents = bpmChangeGridContainer.MapObjects
+                .Where(b => b.JsonTime < currentTime).OrderBy(b => b.JsonTime).Reverse();
+            if (bpmEvents.Count() == 0) return null;
+            return bpmEvents.First();
+        }
+
+        private void SpawnErrorBox(string message)
+        {
+            DialogBox db = PersistentUI.Instance.CreateNewDialogBox();
+            TextComponent text = db.AddComponent<TextComponent>().WithInitialValue(message);
+            db.AddFooterButton(() => {}, "Close");
+        }
+
+        [Exit]
+        private void Exit()
+        {
+            Debug.Log("Application has closed!");
+        }
+    }
+}
---
+++ BPMHelper/UI.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace BPMHelper
+{
+    // Lots of code here is based on/copied from Loloppe's Ratings.UI code
+    internal class UI
+    {
+        public GameObject menu;
+        private BPMHelper bpmHelper;
+        private readonly ExtensionButton extensionButton = new ExtensionButton();
+
+        public UI(BPMHelper bpmHelper)
+        {
+            this.bpmHelper = bpmHelper;
+            ExtensionButtons.AddButton(null, "Open BPM Helper", () => menu.SetActive(!menu.activeSelf));
+        }
+
+        public void AddMenu(MapEditorUI mapEditorUI) {
+            CanvasGroup parent = mapEditorUI.MainUIGroup[5];
+            menu = new GameObject("BPM Helper Menu");
+            menu.transform.parent = parent.transform;
+            AttachTransform(menu, 300, 140, 1, 1, 0, 0, 1, 1);
+            Image image = menu.AddComponent<Image>();
+            image.sprite = PersistentUI.Instance.Sprites.Background;
+            image.type = Image.Type.Sliced;
+            image.color = new Color(0.24f, 0.24f, 0.24f);
+
+            AddButton(parent.transform, "Add Initial BPM", "Add Initial BPM", new Vector2(10, 10), bpmHelper.AddInitialBPM);
+            AddButton(parent.transform, "Add Middle BPM", "Add Middle BPM", new Vector2(20, 20), bpmHelper.AddMiddleBPM);
+            AddButton(parent.transform, "Add Final BPM", "Add Final BPM", new Vector2(30, 30), bpmHelper.AddFinalBPM);
+            AddTextInput(parent.transform, "Number of Beats", "Number of Beats", new Vector2(40, 40), "1", bpmHelper.UpdateNumberOfBeats);
+        }
+
+        private void AddButton(Transform parent, string title, string text, Vector2 pos, UnityAction onClick)
+        {
+            var button = Object.Instantiate(PersistentUI.Instance.ButtonPrefab, parent);
+            MoveTransform(button.transform, 60, 25, 0.5f, 1, pos.x, pos.y);
+
+            button.name = title;
+            button.Button.onClick.AddListener(onClick);
+
+            button.SetText(text);
+            button.Text.enableAutoSizing = false;
+            button.Text.fontSize = 12;
+        }
+
+        private void AddTextInput(Transform parent, string title, string text, Vector2 pos, string value, UnityAction<string> onChange, bool interactable = true, string tooltip = "")
+        {
+            var entryLabel = new GameObject(title + " Label", typeof(TextMeshProUGUI));
+            var rectTransform = ((RectTransform)entryLabel.transform);
+            rectTransform.SetParent(parent);
+
+            MoveTransform(rectTransform, 50, 16, 0.5f, 1, pos.x - 27.5f, pos.y);
+            var textComponent = entryLabel.GetComponent<TextMeshProUGUI>();
+
+            textComponent.name = title;
+            textComponent.font = PersistentUI.Instance.ButtonPrefab.Text.font;
+            textComponent.alignment = TextAlignmentOptions.Right;
+            textComponent.fontSize = 12;
+            textComponent.text = text;
+
+            var textInput = Object.Instantiate(PersistentUI.Instance.TextInputPrefab, parent);
+            MoveTransform(textInput.transform, 55, 20, 0.5f, 1, pos.x + 27.5f, pos.y);
+            textInput.GetComponent<Image>().pixelsPerUnitMultiplier = 3;
+            textInput.InputField.text = value;
+            textInput.InputField.onFocusSelectAll = false;
+            textInput.InputField.textComponent.alignment = TextAlignmentOptions.Left;
+            textInput.InputField.textComponent.fontSize = 10;
+
+            textInput.InputField.onValueChanged.AddListener(onChange);
+            if (!interactable)
+                textInput.InputField.interactable = false;
+
+            if (!string.IsNullOrWhiteSpace(tooltip))
+            {
+                var tt = textInput.InputField.gameObject.AddComponent<Tooltip>();
+                tt.TooltipOverride = tooltip;
+            }
+        }
+
+        private RectTransform AttachTransform(GameObject obj, float sizeX, float sizeY, float anchorX, float anchorY, float anchorPosX, float anchorPosY, float pivotX = 0.5f, float pivotY = 0.5f)
+        {
+            RectTransform rectTransform = obj.AddComponent<RectTransform>();
+            rectTransform.localScale = new Vector3(1, 1, 1);
+            rectTransform.sizeDelta = new Vector2(sizeX, sizeY);
+            rectTransform.pivot = new Vector2(pivotX, pivotY);
+            rectTransform.anchorMin = rectTransform.anchorMax = new Vector2(anchorX, anchorY);
+            rectTransform.anchoredPosition = new Vector3(anchorPosX, anchorPosY, 0);
+
+            return rectTransform;
+        }
+
+        private void MoveTransform(Transform transform, float sizeX, float sizeY, float anchorX, float anchorY, float anchorPosX, float anchorPosY, float pivotX = 0.5f, float pivotY = 0.5f)
+        {
+            if (!(transform is RectTransform rectTransform)) return;
+
+            rectTransform.localScale = new Vector3(1, 1, 1);
+            rectTransform.sizeDelta = new Vector2(sizeX, sizeY);
+            rectTransform.pivot = new Vector2(pivotX, pivotY);
+            rectTransform.anchorMin = rectTransform.anchorMax = new Vector2(anchorX, anchorY);
+            rectTransform.anchoredPosition = new Vector3(anchorPosX, anchorPosY, 0);
+        }
+    }
+}
